dcff4cd836a63c69ead69dcceb89482be4df9ce6
Vladislav Kobyakov <ayakudere@gmail.com>
2018-05-20 19:17:04 +0700

    added new descriptions

---
+++ src/services/WorldState/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var StateKey;
+(function (StateKey) {
+    StateKey["PEOPLE"] = "people";
+    StateKey["MONEY"] = "money";
+    StateKey["INTERNAL_OPINION"] = "internal_opinion";
+    StateKey["EXTERNAL_OPINION"] = "external_opinion";
+})(StateKey = exports.StateKey || (exports.StateKey = {}));
