6a0731474b9a1bf4a77809c46b09071615b77675
Igor Kamyshev <garik.novel@gmail.com>
2018-05-26 11:45:03 +0700

    Added redux and provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import 'reflect-metadata'
 
 import App from './components/App'
+import reducers from './reducers'
+import theme from './theme'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
+    </Provider>,
+    document.getElementById('root'),
+)
---
+++ src/reducers.tsx
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+
+// tslint:disable-next-line:no-empty-interface
+export interface AppState {
+}
+
+export default combineReducers({
+})
